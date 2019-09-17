536cf1c4a5fd4474a824925a0baaa8d61a45f366
v1ceversa <35164682+v1ceversa@users.noreply.github.com>
2019-09-17 22:19:01 +0300

    Initial commit mthrfckr

---
+++ src/by/epam/task_1/stage_1/Sorter.java
@@ -0,0 +1,106 @@
+package by.epam.task_1.stage_1;
+
+import java.lang.reflect.Array;
+
+public class Sorter {
+
+	private static int left(int i) {
+		return 2 * i + 1;
+	}
+
+	private static int right(int i) {
+		return 2 * i + 2;
+	}
+	
+	private static <T> void swap(T[] arr, int i, int j) {
+		T tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	
+	private static <T extends Comparable<T>> void heapify(T[] arr, int n, int i) 
+	{ 
+	    int smallest = i;  
+	    int l = left(i);  
+	    int r = right(i); 
+	   
+	    if (l < n && arr[l].compareTo(arr[smallest]) > 0) {
+	    	smallest = l;  
+	    }
+	    if (r < n && arr[r].compareTo(arr[smallest]) > 0) {
+	    	smallest = r; 
+	    }
+	    
+	    if (smallest != i) { 
+	    	swap(arr, i, smallest);  
+	    	heapify(arr, n, smallest); 
+	    } 
+	} 
+	  
+	
+	private static <T extends Comparable<T>> void buildHeap(T arr[], int n)
+	{
+		for (int i = n / 2 - 1; i >= 0; i--) 
+	        heapify(arr, n, i);
+	}
+	
+	private static <T extends Comparable<T>> void heapSort(T arr[], int n) 
+	{  
+		buildHeap(arr, n);
+		
+	    for (int i = n - 1; i >= 0; i--) {  
+	        swap(arr, 0, i);  
+	        heapify(arr, i, 0); 
+	    } 
+	} 
+	
+	public static <T extends Comparable<T>> void heapSort(T arr[])
+	{
+		heapSort(arr, arr.length);
+	}
+	
+	private static <T extends Comparable<T>> int partition(T[] arr, int p, int r) {
+		T x = arr[r];
+		int i = p - 1;
+		
+		for(int j = p; j < r; j++)
+		{
+			if(arr[j].compareTo(x) <= 0)
+			{
+				i++;
+				swap(arr,i,j);
+			}
+		}
+		swap(arr,i+1,r);
+		return i+1;
+	}
+	
+	public static <T extends Comparable<T>> void qSort(T[] arr, int p, int r) {
+		if(p < r)
+		{
+			int q = partition(arr, p, r);
+			qSort(arr, p, q-1);
+			qSort(arr, q + 1, r);
+		}
+	}
+	
+	public static <T extends Comparable<T>> void qSort(T[] arr) {
+		qSort(arr, 0, arr.length-1);
+	}
+	
+	
+
+	public static <T extends Comparable<T>> void ShellSort (T arr[]) {	
+		
+		for (int step = arr.length / 2; step > 0; step /= 2) {
+			for (int i = step; i < arr.length; i++) {
+				for (int j = i - step; j >= 0 && arr[j].compareTo(arr[j + step]) > 0; j -= step) {
+					swap(arr, j, j+step);
+				}
+			}
+		}
+		
+	}
+	
+	
+}
---
+++ src/by/epam/task_1/stage_1/Task.java
@@ -0,0 +1,19 @@
+package by.epam.task_1.stage_1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task {
+
+	public static void main(String[] args) {
+		Double[] arr = new Double[10];
+		Random rnd = new Random();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rnd.nextDouble()*1000;
+		}
+		System.out.println(Arrays.toString(arr));
+		Sorter.ShellSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
